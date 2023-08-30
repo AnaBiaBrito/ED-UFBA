aced8fb58919c90c6c32784d7867806312a23efb
Ana Bia <anabeatrizbrito03@gmail.com>
2023-08-30 01:28:58 -0300

    criação da função desempilhar

--- Tipos-de-Estrutura/pilha.py
+++ Tipos-de-Estrutura/pilha.py
@@ -6,10 +6,18 @@ class Pilha:
         self.dados = {0} * maxnelems
         self.nelems = 0
 
-# criando função empilha
+# criando função empilha (push)
 def empilha (self, x):
     if(self.nelems == len(self.dados)):
         return False
     self.dados[self.nelems] = x
     self.nelems += 1
     return True
+
+# criando função desempilha (pop)
+def desempilha (self):
+    if(self.nelems == 0):
+        return False
+    self.nelems -=1
+    return True
+# preciso alterar esse código para retornar o que foi retirado da pilha
