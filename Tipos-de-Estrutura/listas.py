e0b5e2f9db81ed20522d08694e3b2b2bbc3aaa54
Ana Bia <anabeatrizbrito03@gmail.com>
2023-08-30 00:23:24 -0300

    criação da função insere

--- Tipos-de-Estrutura/listas.py
+++ Tipos-de-Estrutura/listas.py
@@ -2,4 +2,14 @@
 class Lista:
     def __init__(self, maxnelems):
         self.dados = [0] * maxnelems
-        self.nelemns = 0
+        self.nelems = 0
+
+# agora vamos criar a função consulta
+
+def consulta (self, x):
+    i = 0
+    while (i < self.nelems) and (self.dados[i]!=x):
+        i += 1
+    if (i < self.nelems):
+        return True
+    return False
