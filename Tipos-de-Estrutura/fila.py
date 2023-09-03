66e60f86ecca2993105deabab83d39c9caeaeb94
Ana Bia <anabeatrizbrito03@gmail.com>
2023-09-03 19:54:53 -0300

    fila criada e iniciada a função emfileirar

--- Tipos-de-Estrutura/fila.py
+++ Tipos-de-Estrutura/fila.py
@@ -1,8 +1,15 @@
 # criar fila
 
+def __init__ (self, maxelems):
+    self.dados = [0] * maxelems
+    self.prim = 0
+    self.nelems = 0
 
 # enfilheirar
 
-
+def enfileirar (self, x):
+    if(len(self.dados)) == nelems:
+        return False
+    
 
 # desenfilheirar
