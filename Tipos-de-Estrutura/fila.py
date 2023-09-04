7cf117e4e6486d693ace8ba12f9f4d0e2f5da099
Ana Bia <anabeatrizbrito03@gmail.com>
2023-09-03 22:23:07 -0300

    agr sim

--- Tipos-de-Estrutura/fila.py
+++ Tipos-de-Estrutura/fila.py
@@ -1,15 +1,16 @@
 # criar fila
 
-def __init__ (self, maxelems):
-    self.dados = [0] * maxelems
-    self.prim = 0
-    self.nelems = 0
+class Pilha:
+    def __init__ (self, maxelems):
+        self.dados = [0] * maxelems
+        self.prim = 0
+        self.nelems = 0
 
-# enfilheirar
+# enfileirar
 
 def enfileirar (self, x):
-    if(len(self.dados)) == nelems:
+    if(len(self.dados)) == self.nelems:
         return False
     
 
-# desenfilheirar
+# desenfileirar
